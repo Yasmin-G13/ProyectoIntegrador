09284939406aff1d4a13be92e279f39e906be548
Yasmin-G13 <t23030336@itsch.edu.mx>
2025-05-12 21:58:59 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoIntegrador/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace ProyectoIntegrador
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            colorDialog1 = new ColorDialog();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // colorDialog1
+            // 
+            colorDialog1.Color = Color.SandyBrown;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(370, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(128, 126);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = SystemColors.ActiveCaptionText;
+            label1.Location = new Point(293, 189);
+            label1.Name = "label1";
+            label1.Size = new Size(254, 25);
+            label1.TabIndex = 1;
+            label1.Text = "BIENVENIDO A LA MUEBLERIA";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.Sienna;
+            textBox1.Location = new Point(217, 237);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(372, 31);
+            textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(338, 240);
+            label2.Name = "label2";
+            label2.Size = new Size(140, 25);
+            label2.TabIndex = 3;
+            label2.Text = "INICIAR SESIÓN";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(370, 381);
+            label3.Name = "label3";
+            label3.Size = new Size(108, 25);
+            label3.TabIndex = 5;
+            label3.Text = "REGÍSTRATE";
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.Sienna;
+            textBox2.Location = new Point(217, 378);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(372, 31);
+            textBox2.TabIndex = 4;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(310, 327);
+            label4.Name = "label4";
+            label4.Size = new Size(212, 25);
+            label4.TabIndex = 6;
+            label4.Text = "¿ERES NUEVO USUARIO?";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(682, 2);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(158, 55);
+            pictureBox2.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox2.TabIndex = 7;
+            pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.AntiqueWhite;
+            ClientSize = new Size(852, 417);
+            Controls.Add(pictureBox2);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "INICIO DE SESION ";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ColorDialog colorDialog1;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label4;
+        private PictureBox pictureBox2;
+    }
+}
