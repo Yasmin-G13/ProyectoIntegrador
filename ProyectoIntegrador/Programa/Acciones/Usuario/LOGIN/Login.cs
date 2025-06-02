1c263b63f2b3ee6c9078d3185e3c0405e8886037
Nazareth Bautista <nazarethtello707@gmail.com>
2025-06-01 20:15:56 -0600

    Ya manda al formLogin

--- ProyectoIntegrador/Programa/Acciones/Usuario/LOGIN/Login.cs
+++ ProyectoIntegrador/Programa/Acciones/Usuario/LOGIN/Login.cs
@@ -13,6 +13,10 @@ namespace ProyectoIntegrador.Resources
 {
     public partial class FormLogin : Form
     {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
 
